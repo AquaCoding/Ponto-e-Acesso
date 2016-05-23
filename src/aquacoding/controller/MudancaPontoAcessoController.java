2ad7ad43dbd337f67999387423a05626c08852d3
LeonardoCalado <leonardocalado.uba35@gmail.com>
2016-05-23 08:56:20 -0300

    Mudança ponto - acesso

--- src/aquacoding/controller/MudancaPontoAcessoController.java
+++ src/aquacoding/controller/MudancaPontoAcessoController.java
@@ -4,12 +4,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import aquacoding.pontoacesso.Main;
+import aquacoding.utils.CustomAlert;
 import aquacoding.utils.Serial;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 
 public class MudancaPontoAcessoController implements Initializable {
@@ -23,13 +25,13 @@ public class MudancaPontoAcessoController implements Initializable {
 	@FXML
 	TextField editCOM, statusPontoAcesso;
 	
-	private Serial serial;
+	private Serial serial = Serial.getInstance();
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// Evento de click do botão cancelar		
 		
-		statusCOM.setText("Texto");
+		statusCOM.setText(serial.getPort());
 		
 		if(serial.getStatus()) {
 			statusPontoAcesso.setText("Ponto");
@@ -45,13 +47,18 @@ public class MudancaPontoAcessoController implements Initializable {
 			String COM = editCOM.getText();			
 			serial.setPort(COM);
 			statusCOM.setText(serial.getPort());
+			CustomAlert.showAlert("Mudança Ponto - Acesso", "Porta COM setada com sucesso: "+ statusCOM.getText(), AlertType.INFORMATION);
 		});
 		
 		mudarPontoAcesso.setOnMouseClicked((MouseEvent e) -> {
 			if(statusPontoAcesso.getText().equals("Ponto")) {
 				serial.setStatus(false);
+				statusPontoAcesso.setText("Acesso");
+				CustomAlert.showAlert("Mudança Ponto - Acesso", "Sistema alterado para registro de acesso", AlertType.INFORMATION);
 			}else {
 				serial.setStatus(true);
+				statusPontoAcesso.setText("Ponto");
+				CustomAlert.showAlert("Mudança Ponto - Acesso", "Sistema alterado para registro de ponto", AlertType.INFORMATION);
 			}
 		});
 		
